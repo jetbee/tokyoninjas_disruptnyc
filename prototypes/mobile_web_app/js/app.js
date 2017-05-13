436bb968476befa918a6b9054525f7c13687cb15
jetbee <gen@j37b-mbp15-2.local>
2017-05-13 16:24:38 -0400

    view prototypes

---
+++ prototypes/mobile_web_app/js/app.js
@@ -0,0 +1,11 @@
+// routing test
+var MainApp = angular.module('MainApp', ['ngRoute']);
+
+MainApp.config(function($routeProvider){
+	$routeProvider
+	  .when('/', {controller: 'mainsCtrl', templateUrl:'view01.html'})
+	  .when('/view01', {controller: 'mainsCtrl', templateUrl:'view01.html'})
+	  .when('/view02', { controller: 'mainsCtrl', templateUrl: 'view02.html'})
+	  .otherwise({redirectTo:'/'});
+});
+
